7fec336e08372cf0c15b5b6bc36457e78ee1aee0
Deep Patel <deeppatel@Deeps-MacBook-Air.local>
2023-01-10 12:36:31 +0530

    changed to JPA repository for confirmation token

--- src/main/java/com/example/registration/repository/ConfirmationTokenRepository.java
+++ src/main/java/com/example/registration/repository/ConfirmationTokenRepository.java
@@ -1,10 +1,11 @@
 package com.example.registration.repository;
 
 import com.example.registration.entity.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("confirmationTokenRepository")
-public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken, String> {
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
     ConfirmationToken findByConfirmationToken(String confirmationToken);
 }
